d46a8038b2cf0c04c25ef06420076a970c4f9b25
svaronc <sebastianvaron96@gmail.com>
2024-01-28 00:47:58 -0500

    call the api for data and update the states

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,42 +1,74 @@
 import React, { Component } from "react";
-
+import Axios from "axios";
 import classnames from "classnames";
 import { Loading } from "./Loading";
 import { Panel } from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads,
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10,
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4,
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng",
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza",
+    getValue: getUserWithLeastUploads
   },
 ];
 class Dashboard extends Component {
   state = {
-    loading: false,
+    loading: true,
     focused: null,
+    photos: [],
+    topics: [],
   };
+  componentDidMount() {
+    const urlPromise = ["/api/photos", "/api/topics"].map((url) =>
+      Axios.get(url).then((response) => response.data)
+    );
+    Promise.all(urlPromise).then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics,
+      });
+    });
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
   selecPanel(id) {
     this.setState((prev) => ({
       focused: prev.focused !== null ? null : id,
     }));
   }
   render() {
+    console.log(this.state);
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused,
     });
@@ -52,7 +84,7 @@ class Dashboard extends Component {
         key={item.id}
         id={item.id}
         label={item.label}
-        value={item.value}
+        value={item.getValue(this.state)}
         onSelect={(event) => this.selecPanel(item.id)}
       />
     ));
